92715e08ea27348d59e8af5a8a206ced3ec98310
JohnCoronel <john.m.coronel@gmail.com>
2018-04-15 01:59:49 -0700

    movieCard reveal info button working  -TODO - fix info styling

--- src/card.js
+++ src/card.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ClassNames from 'classnames';
+
 import "./style/card.css";
 import AddIcon from  "./components/AddIcon";
 import LikeIcon from "./components/LikeIcon";
@@ -10,29 +12,52 @@ class MovieCard extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            clicked: false
+            expanded: false
         };
     }
+
+    
+    showContent = () => {
+        this.setState({expanded:!this.state.expanded})
+    }
+
     render() {
+        let chevronClass = ClassNames({
+            'expand-btn':true,
+            'open':this.state.expanded,
+        })
+
+        let content = ClassNames({
+            'wrapper':true,
+            'content-shown':this.state.expanded
+        })
+
+        let bodyClass = ClassNames({
+            'content--body':true,
+            'content--body-hidden':!this.state.expanded,
+            'content-body-active':this.state.expanded
+        })
         return (
             <div className = "movie_card">  
                 <img src = "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt = "movie poster"></img>
-                <section className = "card--content">
+                <div className = "card--content">
+                <div className ={content}>
                     <div className = "content--header">
                     <div className = "content--header-top">
                     <h3> Mad Max Fury Road 
 
                           </h3>
-                    <div className="show-card-body-btn"><ChevronIcon/></div>
+                    <div  onClick = {this.showContent} className={chevronClass}><ChevronIcon/></div>
                     </div>
                 
                     <h5> Action | Sci-Fi ‧ 2hr 15m ‧ 2015  </h5>
                     </div>
-                    <div className = "content--body">
-                    <p> "Lorem ipsum dolsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    <div className ={bodyClass}>
+                     "Lorem ipsum dolsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteu."
-                    </p>
+                    
+                    </div>
                     </div>
                     <div className = "content--footer">
                     <AddIcon/>
@@ -40,7 +65,7 @@ class MovieCard extends React.Component {
                     <ShareIcon/>
                     <ReviewIcon/>
                     </div>
-                </section>
+                </div>
 
             </div>
         )
--- src/movie.js
+++ src/movie.js
@@ -6,8 +6,13 @@ import MovieCard from './card';
 const Movie = ({match}) => (
     <div className = "content">
         This is the page for the {match.params.id}
+        <div style = {{display:'flex'}}>
+        <MovieCard />
         <MovieCard/>
+           d</div>
         </div>
+    
+
 )
 
 export default Movie;
